f37498eb6594349502c64608431811092986745a
Alvaro Tedeschi <alvarotedeschi@gmail.com>
2023-08-05 17:20:29 -0300

    add update and delete company endpoints

--- src/controllers/company_controller.ts
+++ src/controllers/company_controller.ts
@@ -20,3 +20,30 @@ export async function getCompanies(_req: Request, res: Response) {
         res.status(500).json({ error: 'Could not retrieve companies.' });
     }
 }
+
+export async function updateCompany(req: Request, res: Response) {
+    try {
+        const { id } = req.params;
+        const { name } = req.body;
+        const updatedCompany = await Company.findByIdAndUpdate(id, { name }, { new: true });
+        if (!updatedCompany) {
+            return res.status(404).json({ error: 'Company not found.' });
+        }
+        res.json(updatedCompany);
+    } catch (err) {
+        res.status(500).json({ error: 'Could not update the company.' });
+    }
+}
+
+export async function deleteCompany(req: Request, res: Response) {
+    try {
+        const { id } = req.params;
+        const deletedCompany = await Company.findByIdAndDelete(id);
+        if (!deletedCompany) {
+            return res.status(404).json({ error: 'Company not found.' });
+        }
+        res.json(deletedCompany);
+    } catch (err) {
+        res.status(500).json({ error: 'Could not delete the company.' });
+    }
+}
--- src/routes/company_routes.ts
+++ src/routes/company_routes.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-import { createCompany, getCompanies } from '../controllers/company_controller';
+import { createCompany, deleteCompany, getCompanies, updateCompany } from '../controllers/company_controller';
 
 const router = express.Router();
 
 router.post('/', createCompany);
 router.get('/', getCompanies);
+router.put('/:id', updateCompany);
+router.delete('/:id', deleteCompany);
 
 export default router;
