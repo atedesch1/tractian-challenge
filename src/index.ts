779fc9ed82da0b7eccf639ef6193ea70cb60d31e
Alvaro Tedeschi <alvarotedeschi@gmail.com>
2023-08-10 14:05:43 -0300

    update server is running log

--- src/index.ts
+++ src/index.ts
@@ -33,5 +33,5 @@ import router from './router';
 app.use(router);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
